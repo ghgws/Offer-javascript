3d89d4b13137e542f341d0df71f7bc8a12f2f6fe
ghgws <2897996242@qq.com>
2022-04-17 13:52:21 +0800

    Create 05替换空格.js

---
+++ 数组/05替换空格.js
@@ -0,0 +1,22 @@
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ * @param {string} s
+ * @return {string}
+ */
+// 方法一：遍历字符串: 利用字符串实际上是字符类型的数组来处理（但是不能在字符串原地修改）
+var replaceSpace = function(s) {
+  let res = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      res += "%20";
+      continue;
+    }
+    res += s[i];
+  }
+  return res;
+};
+
+// 方法二：正则替换
+var replaceSpace = function(s) {
+  return s.replace(/\s/g, "%20");
+};
